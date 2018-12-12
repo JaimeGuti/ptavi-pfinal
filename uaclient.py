daf27f24861709a4ff7ebfc8c8d3861bca3a4a8c
JaimeGuti <jaime.m.guti@gmail.com>
2018-12-12 13:32:01 +0100

    Definida la clase en uaclient.py

---
+++ uaclient.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import socket
+import sys
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+
+        self.attrs = []
+        self.tags = []
+
+    def startElement(self, name, attrs):
+
+        if name == 'account':
+
+            acc = {}
+            acc['username'] = attrs.get('username', "")
+            acc['passwd'] = attrs.get('passwd', "")
+
+            self.tags.append(['account', acc])
+
+        elif name == 'uaserver':
+
+            uas = {}
+            uas['ip'] = attrs.get('ip', "")
+            uas['puerto'] = attrs.get('puerto', "")
+
+            self.tags.append(['uaserver', uas])
+
+        elif name == 'rtpaudio':
+
+            rtp = {}
+            rtp['puerto'] = attrs.get('puerto', "")
+
+            self.tags.append(['rtpaudio', rtp])
+
+        elif name == 'regproxy':
+
+            regp = {}
+            regp['ip'] = attrs.get('ip', "")
+            regp['puerto'] = attrs.get('puerto', "")
+
+            self.tags.append(['regproxy', regp])
+
+        elif name == 'log':
+
+            lg = {}
+            lg['path'] = attrs.get('path', "")
+
+            self.tags.append(['log', lg])
+
+        elif name == 'audio':
+
+            aud = {}
+            aud['path'] = attrs.get('path', "")
+
+            self.tags.append(['audio', aud])
+
+    def get_tags(self):
+
+        return self.tags
+
+try:
+    METODO = sys.argv[1].upper()
+    RECEPTOR = sys.argv[2].split("@")[0]
+    IP = sys.argv[2].split("@")[1].split(":")[0]
+    SIPPORT = int(sys.argv[2].split(":")[-1])
+
+    # Contenido que vamos a enviar
+    LINE = METODO + " sip:" + RECEPTOR + "@" + IP + " SIP/2.0\r\n\r\n"
+
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IP, SIPPORT))
+
+        if METODO == "INVITE":
+            print("Enviando: " + LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+
+        elif METODO == "BYE":
+            print("Enviando: " + LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+
+        mens_ack = data.decode('utf-8')
+        if mens_ack == "SIP/2.0 100 Trying SIP/2.0 180 Ringing SIP/2.0 200 OK":
+            ack_line = 'ACK' + " sip:" + RECEPTOR + "@"
+            ack_line += IP + " SIP/2.0\r\n\r\n"
+            print(ack_line)
+            my_socket.send(bytes(ack_line, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+
+        print('Recibido -- ', data.decode('utf-8'))
+        print("Terminando socket...")
+
+    print("Fin.")
+except:
+    sys.exit("Usage: python uaclient.py config method option")
---
+++ uaserver.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import socketserver
+import sys
+import os
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    AUDIO = sys.argv[3]
+except:
+    sys.exit("Usage: python uaserver.py config")
+
+
+class EchoHandler(socketserver.DatagramRequestHandler):
+
+    def handle(self):
+
+        line = self.rfile.read().decode('utf-8').split()[0]
+        print("El cliente nos manda " + line)
+        diferente = (line != "INVITE") or (line != "BYE") or (line != "ACK")
+
+        if line == "INVITE":
+            self.wfile.write(b"SIP/2.0 100 Trying")
+            self.wfile.write(b" SIP/2.0 180 Ring")
+            self.wfile.write(b" SIP/2.0 200 OK")
+
+        elif line == "ACK":
+            aEjecutar = "./mp32rtp -i " + receptor_IP + " -p "
+            aEjecutar += receptor_Puerto + " < " + AUDIO
+            print("Vamos a ejecutar", aEjecutar)
+            os.system(aEjecutar)
+"""
+        elif line == "BYE":
+            self.wfile.write(b"SIP/2.0 200 OK")
+
+        elif line == ""
+            #Usuario se intenta registrar sin autenticarse
+            self.wfile.write(b"SIP/2.0 401 Unauthorized")
+
+        elif line == ""
+            #Usuario no encontrado en el servidor de registro
+            self.wfile.write(b"SIP/2.0 404 User Not Found")
+"""
+        elif diferente:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed")
+
+        else:
+            self.wfile.write(b"SIP/2.0 400 Bad Request")
+
+if __name__ == "__main__":
+    # Creamos servidor de eco y escuchamos
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Servidor apagado")
