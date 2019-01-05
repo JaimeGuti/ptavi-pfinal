521f4ec2349d64a3ce7a3a91dae96a047de64fbf
JaimeGuti <jaime.m.guti@gmail.com>
2019-01-05 18:25:29 +0100

    Añadida la clase en proxy_registrar

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -12,6 +12,43 @@ import time
 from uaclient import log_fich
 
 
+class PxReg_XMLHandler(ContentHandler):
+
+    def __init__(self):
+
+        self.attrs = []
+        self.tags = {}
+
+    def startElement(self, name, attrs):
+
+        if name == 'server':
+
+            acc = {}
+            acc['name'] = attrs.get('name', "")
+            acc['ip'] = attrs.get('ip', "")
+            acc['puerto'] = attrs.get('puerto', "")
+
+            self.tags['account'] = acc
+
+        elif name == 'database':
+
+            uas = {}
+            uas['path'] = attrs.get('path', "")
+            uas['passwdpath'] = attrs.get('passwdpath', "")
+
+            self.tags['uaserver'] = uas
+
+        elif name == 'log':
+
+            rtp = {}
+            rtp['path'] = attrs.get('path', "")
+
+            self.tags['rtpaudio'] = rtp
+
+    def get_tags(self):
+
+        return self.tags
+
 if __name__ == "__main__":
 
     try:
